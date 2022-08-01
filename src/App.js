ed9ea16edc34f2dbf7346813ab1d97c6e41ba26b
shubham <patankarshubham2595@gmail.com>
2022-08-01 11:29:03 +0530

    commit-15

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <header className="App-header" >
+        <img src={logo} className="App-logo" alt="logo"  />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload the page.
         </p>
         <a
           className="App-link"
